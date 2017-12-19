390ef86576e1095c41bcdd4e45b573eb6e08d27e
crystalDf <chendong333@gmail.com>
2017-12-19 14:48:04 +0800

    Chapter 20 BeatBox

--- app/src/main/java/com/star/beatbox/BeatBox.java
+++ app/src/main/java/com/star/beatbox/BeatBox.java
@@ -23,19 +23,19 @@ public class BeatBox {
     }
 
     private void loadSounds() {
-        String[] soundNames;
+        String[] soundNames = new String[0];
         try {
             soundNames = mAssetManager.list(SOUNDS_FOLDER);
-
-            for (String filename : soundNames) {
-                String assetPath = SOUNDS_FOLDER + "/" + filename;
-                Sound sound = new Sound(assetPath);
-                mSounds.add(sound);
-            }
 
             Log.i(TAG, "Found " + soundNames.length + " sounds");
         } catch (IOException e) {
             Log.e(TAG, "Could not list assets", e);
+        }
+
+        for (String filename : soundNames) {
+            String assetPath = SOUNDS_FOLDER + "/" + filename;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
         }
     }
 
